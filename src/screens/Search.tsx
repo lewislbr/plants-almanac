2535dd115a93dfce2f9e6147a0e67ec8552f519b
Lewis Llobera <billobera@gmail.com>
2019-08-20 12:31:42 +0200

    Add API call

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import 'regenerator-runtime/runtime';
 
 import { PlantCard } from '../components';
 
 const Search = () => {
+  const API_KEY = process.env.API_KEY;
+
+  const [query, setQuery] = useState('rosemary');
+
+  const getData = async () => {
+    const response = await fetch(
+      `https://trefle.io/api/plants?q=${query}?token=${API_KEY}`
+    );
+    const data = response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <div>
