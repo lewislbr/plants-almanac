3644728c505c17d16608a8960a1cc37b91ead630
Lewis Llobera <billobera@gmail.com>
2019-08-20 13:21:01 +0200

    Add form logic

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -6,6 +6,8 @@ import { PlantCard } from '../components';
 const Search = () => {
   const API_KEY = process.env.API_KEY;
 
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState('');
   const [query, setQuery] = useState('rosemary');
 
   const getData = async () => {
@@ -13,24 +15,38 @@ const Search = () => {
       `https://trefle.io/api/plants?q=${query}?token=${API_KEY}`
     );
     const data = response.json();
+    setPlants(data);
     console.log(data);
   };
 
   useEffect(() => {
     getData();
-  }, []);
+    console.log('useEffect here');
+  }, [query]);
+
+  const updateSearch = (event: any) => {
+    setSearch(event.target.value);
+    console.log('updateSearch here');
+  };
+
+  const updateQuery = (event: any) => {
+    event.preventDefault();
+    setQuery(search);
+  };
 
   return (
     <>
       <div>
         <h1>Search</h1>
       </div>
-      <form>
-        <input type="text" />
+      <form onSubmit={updateQuery}>
+        <input type="text" value={search} onChange={updateSearch} />
         <button type="submit">Search</button>
       </form>
       <div>
-        <PlantCard />
+        {plants.map((plant) => (
+          <PlantCard />
+        ))}
       </div>
     </>
   );
