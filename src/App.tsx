f2548b90feb9484d9e4c979631e3be8d32a7a95b
Lewis Llobera <billobera@gmail.com>
2019-08-20 12:10:12 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { Home, Search } from './screens';
 
 const App = () => {
-  return <h1>Project Name</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/search" component={Search} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
