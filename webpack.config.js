39215e96b071e4393eba14ed629aaa4a88cc6731
Lewis Llobera <billobera@gmail.com>
2019-08-20 12:10:35 +0200

    Format

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   entry: './src/index',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'main.js'
+    filename: 'main.js',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
@@ -15,14 +17,23 @@ module.exports = {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+      template: './src/index.html',
+    }),
+    new Dotenv(),
+  ],
+  stats: {
+    errorDetails: true,
+  },
+  devServer: {
+    contentBase: __dirname + '/public/',
+    port: 3000,
+    historyApiFallback: true,
+  },
 };
