565f863a775a8ee49d0e074caa3af80764b6a229
Lewis Llobera <billobera@gmail.com>
2019-08-22 19:55:29 +0200

    Add graphql test schema

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,28 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+const schema = buildSchema(`
+  type Query {
+    plant: String
+  }
+`);
 
-app.listen(8080, () => {
-  console.log('Listening');
-});
+const root = {
+  plant: () => {
+    return 'ROSEMARY';
+  },
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4040);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
