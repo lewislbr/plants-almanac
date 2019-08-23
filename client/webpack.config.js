d5da1539df9c908ef7831929b77547847c8cbef8
Lewis Llobera <billobera@gmail.com>
2019-08-23 18:36:41 +0200

    Refactor as import

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import Dotenv from 'dotenv-webpack';
 
 module.exports = {
   target: 'web',
