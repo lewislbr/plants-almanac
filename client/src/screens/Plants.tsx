806788b785935a82cbf4f8d3b93d364a5aba671c
Lewis Llobera <billobera@gmail.com>
2019-08-26 09:26:32 +0200

    Fix unnecessary api call on render

--- client/src/screens/Plants.tsx
+++ client/src/screens/Plants.tsx
@@ -1,38 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { PlantCard } from '../components';
 
 const Plants: React.FunctionComponent = () => {
   const [plants, setPlants] = useState([]);
-  const [search, setSearch] = useState('');
-  const [query, setQuery] = useState('rosemary');
+  const [input, setInput] = useState('');
+  const [query, setQuery] = useState('');
 
-  useEffect(() => {
-    const getData = async (): Promise<void> => {
-      const TREFLE_TOKEN = process.env.TREFLE_TOKEN;
+  const getData = async (): Promise<void> => {
+    const TREFLE_TOKEN = process.env.TREFLE_TOKEN;
 
-      const response = await fetch(
-        `https://trefle.io/api/plants?q=${query}?token=${TREFLE_TOKEN}`
-      );
-      const data = await response.json();
-      setPlants(data);
-      console.log(data);
-    };
-    getData();
-    console.log('useEffect here');
-  }, [query]);
-
-  const updateSearch = (event: React.FormEvent<HTMLInputElement>): void => {
-    const target = event.target as HTMLInputElement;
-    setSearch(target.value);
-    console.log('updateSearch here');
+    const response = await fetch(
+      `https://trefle.io/api/plants?q=${query}?token=${TREFLE_TOKEN}`
+    );
+    const data = await response.json();
+    setPlants(data);
+    console.log(data);
   };
 
-  const updateQuery = (event: React.FormEvent<HTMLFormElement>): void => {
-    if (!search) return event.preventDefault();
+  const updateInput = (event: React.FormEvent<HTMLInputElement>): void => {
+    const target = event.target as HTMLInputElement;
+    setInput(target.value);
+    console.log('updateInput here');
+  };
+
+  const makeQuery = (event: React.FormEvent<HTMLFormElement>): void => {
+    if (!input) return event.preventDefault();
     event.preventDefault();
-    setQuery(search);
+    setQuery(input);
+    getData();
   };
 
   return (
@@ -40,8 +37,8 @@ const Plants: React.FunctionComponent = () => {
       <div>
         <h1>Search</h1>
       </div>
-      <form onSubmit={updateQuery}>
-        <input type="text" value={search} onChange={updateSearch} />
+      <form onSubmit={makeQuery}>
+        <input type="text" value={input} onChange={updateInput} />
         <button type="submit">Search</button>
       </form>
       <div>
