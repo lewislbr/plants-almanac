fe0ebc05b5599a18dc9c0d028f1632fb2b65d8a0
Lewis Llobera <billobera@gmail.com>
2019-09-01 00:50:13 +0200

    Fix typo

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
