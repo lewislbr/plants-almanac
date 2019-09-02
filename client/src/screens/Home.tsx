8ceb6ec7021ccd85668c26f9a52332892696f4a2
Lewis Llobera <billobera@gmail.com>
2019-09-02 15:31:42 +0200

    Create query

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import { PlantCard } from '../components';
+
+const GET_PLANTS = gql`
+  query {
+    getPlants {
+      _id
+      name
+    }
+  }
+`;
+
+interface Plant {
+  _id: string;
+  name: string;
+  description: string;
+  plantSeason: string[];
+  harvestSeason: string[];
+  pruneSeason: string[];
+  tips: string;
+}
 
 export const Home: React.FunctionComponent = () => {
+  const { data, loading, error } = useQuery(GET_PLANTS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>ERROR</p>;
+
   return (
-    <div>
+    <>
       <h1>Home</h1>
-    </div>
+      {data.getPlants.map((plant: Plant) => (
+        <PlantCard key={plant._id} name={plant.name} />
+      ))}
+    </>
   );
 };
