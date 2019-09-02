2d6860af81f3fecd6126fffcdf3eaa198cef8e72
Lewis Llobera <billobera@gmail.com>
2019-09-02 15:30:17 +0200

    Connect apollo client to react

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4040/',
+});
+
+const client = new ApolloClient({
+  cache,
+  link,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
