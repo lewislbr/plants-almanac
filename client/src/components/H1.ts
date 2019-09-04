f7ad823d9e08780834e175f01892437df8c2933c
Lewis Llobera <billobera@gmail.com>
2019-09-04 22:22:59 +0200

    Edit color

--- client/src/components/H1.ts
+++ client/src/components/H1.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const H1 = styled.h1`
-  color: var(--color-accent-secondary);
+  color: var(--color-accent-primary);
   font-size: var(--font-size-xl);
 `;
