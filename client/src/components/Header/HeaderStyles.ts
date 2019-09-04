5f0d5ade159efeb22268dbced1d9892b07187776
Lewis Llobera <billobera@gmail.com>
2019-09-04 10:55:55 +0200

    Add component styles

---
+++ client/src/components/Header/HeaderStyles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  height: 60px;
+  justify-content: space-between;
+  left: 0%;
+  padding: 0px var(--padding-desktop);
+  position: fixed;
+  right: 0;
+  top: 0;
+`;
+
+export const Li = styled.li`
+  margin-left: 30px;
+`;
+
+export const Nav = styled.nav`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Ul = styled.ul`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+`;
