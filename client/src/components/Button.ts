4bbbf1e126cf0ebfb1d593df643b15020446fd8e
Lewis Llobera <billobera@gmail.com>
2019-09-04 22:22:35 +0200

    Add style property

--- client/src/components/Button.ts
+++ client/src/components/Button.ts
@@ -9,6 +9,9 @@ export const Button = styled.button`
   background-color: ${(props: Props): string =>
     props.primary ? 'var(--color-accent-primary)' : 'var(--color-light)'};
   border-radius: var(--radius-m);
+  color: ${(props: Props): string =>
+    props.primary ? 'var(--color-light)' : 'var(--color-accent-primary)'};
+  cursor: pointer;
   margin: var(--spacing-xs);
   padding: var(--spacing-xxs) var(--spacing-s);
 `;
