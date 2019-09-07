b07adc4b7aa2379afda50450294dfb41b20e955a
Lewis Llobera <billobera@gmail.com>
2019-09-07 15:43:03 +0200

    Edit loading component layout

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  padding-top: 10%;
+`;
+
 const spin = keyframes`
   0% {
     transform: rotate(0deg);
@@ -12,22 +19,22 @@ const spin = keyframes`
 
 const Div1 = styled.div`
   display: inline-block;
+  height: 64px;
   position: relative;
   width: 64px;
-  height: 64px;
 `;
 
 const Div2 = styled.div`
+  animation: ${spin} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border: 6px solid var(--color-accent-primary);
+  border-color: var(--color-accent-primary) transparent transparent transparent;
+  border-radius: 50%;
   box-sizing: border-box;
   display: block;
-  position: absolute;
-  width: 51px;
   height: 51px;
   margin: 6px;
-  border: 6px solid var(--color-accent-primary);
-  border-radius: 50%;
-  animation: ${spin} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: var(--color-accent-primary) transparent transparent transparent;
+  position: absolute;
+  width: 51px;
 `;
 
 const Div3 = styled(Div2)`
@@ -40,11 +47,13 @@ const Div4 = styled(Div2)`
 
 export const Loading: React.FunctionComponent = () => {
   return (
-    <Div1>
-      <Div2></Div2>
-      <Div3></Div3>
-      <Div4></Div4>
-      <Div2></Div2>
-    </Div1>
+    <Wrapper>
+      <Div1>
+        <Div2></Div2>
+        <Div3></Div3>
+        <Div4></Div4>
+        <Div2></Div2>
+      </Div1>
+    </Wrapper>
   );
 };
