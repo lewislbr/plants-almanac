f5a3cbd03499e599c76936bcd43fe3aed7110169
Lewis Llobera <billobera@gmail.com>
2019-12-03 12:36:33 +0100

    Update webpack configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,68 +1,103 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
 
-module.exports = {
-  target: 'web',
-  entry: './src/index',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-    filename: '[name].[contenthash].js',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env, options) => {
+  const isDevelopment = options.mode !== 'production';
+
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    target: 'web',
+    entry: './src/index.tsx',
+    output: {
+      filename: isDevelopment ? '[name].js' : '[name].[contenthash].js',
+      path: path.join(__dirname, '/dist'),
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
-      },
-    ],
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    moduleIds: 'hashed',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    devtool: isDevelopment
+      ? 'cheap-module-eval-source-map'
+      : 'cheap-module-source-map',
+    optimization: {
+      moduleIds: 'hashed',
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            name: 'vendors',
+            test: /[\\/]node_modules[\\/]/,
+          },
         },
       },
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new Dotenv(),
-  ],
-  stats: {
-    assetsSort: '!size',
-    builtAt: false,
-    children: false,
-    entrypoints: false,
-    errors: true,
-    errorDetails: true,
-    hash: false,
-    modules: false,
-    timings: false,
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 8080,
-    open: 'Google Chrome',
-    historyApiFallback: true,
-  },
+    performance: {
+      hints: isDevelopment ? 'warning' : 'error',
+      maxEntrypointSize: 300_000,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        minify: isDevelopment
+          ? false
+          : {
+              collapseWhitespace: true,
+              minifyCSS: true,
+              minifyJS: true,
+              minifyURLs: true,
+              removeComments: true,
+              removeRedundantAttributes: true,
+              removeScriptTypeAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              useShortDoctype: true,
+            },
+        template: './src/index.html',
+      }),
+      ...(isDevelopment
+        ? []
+        : [
+            new CompressionPlugin({
+              algorithm: 'brotliCompress',
+              compressionOptions: { level: 11 },
+              filename: '[path].br[query]',
+              test: /\.(js|css|html|svg)$/,
+            }),
+          ]),
+    ],
+    stats: {
+      assetsSort: '!size',
+      builtAt: false,
+      children: false,
+      entrypoints: false,
+      errors: true,
+      errorDetails: true,
+      hash: false,
+      modules: false,
+      timings: false,
+    },
+    devServer: {
+      contentBase: 'dist',
+      historyApiFallback: true,
+      hot: true,
+      open: true,
+      port: 8080,
+    },
+  };
 };
