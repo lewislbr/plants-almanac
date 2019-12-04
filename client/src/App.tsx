2427c932b514ab5339941f2f0a2397572ca3c291
Lewis Llobera <billobera@gmail.com>
2019-12-04 18:14:28 +0100

    Delete view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { AddPlant, Home, PlantDetails, Search } from './screens';
+import { AddPlant, Home, PlantDetails } from './screens';
 
 import { GlobalStyles, Header, Main } from './components';
 
@@ -16,7 +16,6 @@ export const App: React.FunctionComponent = () => {
             <Route exact path="/" component={Home} />
             <Route path="/home" component={Home} />
             <Route path="/addplant" component={AddPlant} />
-            <Route exact path="/search" component={Search} />
             <Route path="/:plantname" component={PlantDetails} />
           </Switch>
         </Main>
--- client/src/screens/index.ts
+++ client/src/screens/index.ts
@@ -1,4 +1,3 @@
 export { AddPlant } from './AddPlant/AddPlant';
 export { Home } from './Home';
 export { PlantDetails } from './PlantDetails';
-export { Search } from './Search';
