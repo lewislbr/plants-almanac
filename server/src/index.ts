e927d593d833a6b81282645b4d466bb17c2d49b0
Lewis Llobera <billobera@gmail.com>
2019-12-12 14:39:23 +0100

    Refactor server to apollo-server-express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,34 @@
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import mongoose from 'mongoose';
 
 import { typeDefs } from './graphql/typeDefs';
 import { resolvers } from './graphql/resolvers';
 
-const startServer = async (): Promise<void> => {
-  const server = new ApolloServer({
-    cors: {
-      origin: '*',
-      methods: 'POST, GET, OPTIONS',
-    },
-    typeDefs,
-    resolvers,
-    engine: {
-      apiKey: process.env.ENGINE_API_KEY,
-    },
-  });
+const app = express();
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+const startServer = async (): Promise<void> => {
   await mongoose
     .connect(
       `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0-ovl3w.mongodb.net/${process.env.MONGODB_DB}?retryWrites=true&w=majority`,
       { useNewUrlParser: true, useUnifiedTopology: true }
     )
+    .then(() => console.log('MongoDB connected'))
     .catch((error) => console.log(error));
 
-  server.listen({ port: 4040 }).then(({ url }) => {
-    console.log(`Server ready at ${url} 🚀`);
+  const PORT = process.env.PORT || 4040;
+
+  app.listen(PORT, () => {
+    console.log(
+      `Server ready at http://localhost:${PORT}${server.graphqlPath} 🚀`
+    );
   });
 };
 
