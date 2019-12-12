e16773e205cc01be3921bf1f04fe80037d69c3ce
Lewis Llobera <billobera@gmail.com>
2019-12-12 15:08:42 +0100

    Update GraphQL API URI

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,7 +9,7 @@ import { App } from './App';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: 'http://localhost:4040/',
+  uri: 'http://localhost:4040/graphql',
 });
 
 const client = new ApolloClient({
