b3d7142bb587f2203253a30b7cf1d7164714946c
Lewis Llobera <billobera@gmail.com>
2020-06-29 18:02:30 +0200

    Rename service

--- plants/src/main.ts
+++ plants/src/main.ts
@@ -64,7 +64,9 @@ async function startServer(): Promise<void> {
     server.get("/playground", expressPlayground({endpoint: "/graphql"}));
   }
 
-  server.listen(port, () => console.log(`Data ready at localhost:${port} ✅`));
+  server.listen(port, () =>
+    console.log(`Plants service ready at localhost:${port} ✅`),
+  );
 }
 
 startServer();
