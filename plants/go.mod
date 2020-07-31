6c8a3c012a14c6abdbb42893773333931ccef2c0
Lewis Llobera <billobera@gmail.com>
2020-07-31 12:29:46 +0200

    Refactor GraphQL handler

--- plants/go.mod
+++ plants/go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/graphql-go/graphql v0.7.9
 	github.com/graphql-go/handler v0.2.3
 	github.com/joho/godotenv v1.3.0
+	github.com/julienschmidt/httprouter v1.3.0
 	go.mongodb.org/mongo-driver v1.4.0
 )
--- plants/pkg/http/graphql/handler.go
+++ plants/pkg/http/graphql/handler.go
@@ -3,24 +3,36 @@ package graphql
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/graphql-go/handler"
+	"github.com/joho/godotenv"
+	"github.com/julienschmidt/httprouter"
 )
 
-// Start initalizes the endpoint where GraphQL is available
+// Start initalizes the GraphQL endpoints
 func Start() error {
-	handler := handler.New(&handler.Config{
+	godotenv.Load()
+
+	router := httprouter.New()
+	port := os.Getenv("PORT")
+	graphqlHandler := handler.New(&handler.Config{
+		Schema:     &schema,
+		Pretty:     false,
+		Playground: false,
+	})
+	playgroundHandler := handler.New(&handler.Config{
 		Schema:     &schema,
 		Pretty:     true,
-		GraphiQL:   false,
 		Playground: true,
 	})
 
-	port := "4040"
+	router.POST("/api", httpWrapper(graphqlHandler))
+	router.GET("/api/playground", httpWrapper(playgroundHandler))
 
 	fmt.Printf("Server ready at http://localhost:%v ✅\n", port)
 
-	err := http.ListenAndServe(":"+port, corsHandler(handler))
+	err := http.ListenAndServe(":"+port, corsWrapper(router))
 	if err != nil {
 		return err
 	}
@@ -28,7 +40,13 @@ func Start() error {
 	return nil
 }
 
-func corsHandler(h http.Handler) http.HandlerFunc {
+func httpWrapper(h http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		h.ServeHTTP(w, r)
+	}
+}
+
+func corsWrapper(h http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Origin")
 		w.Header().Add("Access-Control-Max-Age", "86400")
