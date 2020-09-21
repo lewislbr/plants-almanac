7580fc845b49be1e8aa1da3e81d5746fbcc1aa67
Lewis Llobera <billobera@gmail.com>
2020-09-21 16:07:26 +0200

    Enable code splitting

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,41 @@
 import * as React from "react";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Header} from "./components";
-import {AddPlant, PlantDetails, Plants} from "./views";
+
+const AddPlant = React.lazy(() =>
+  import("./views/AddPlant" /* webpackChunkName: 'AddPlant' */).then(
+    ({AddPlant}) => ({
+      default: AddPlant,
+    }),
+  ),
+);
+const PlantDetails = React.lazy(() =>
+  import("./views/PlantDetails" /* webpackChunkName: 'PlantDetails' */).then(
+    ({PlantDetails}) => ({
+      default: PlantDetails,
+    }),
+  ),
+);
+const Plants = React.lazy(() =>
+  import("./views/Plants" /* webpackChunkName: 'Plants' */).then(
+    ({Plants}) => ({
+      default: Plants,
+    }),
+  ),
+);
 
 export function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Header />
       <main className="max-w-5xl mx-auto p-5pc">
-        <Switch>
-          <Route exact path="/" component={Plants} />
-          <Route path="/addplant" component={AddPlant} />
-          <Route path="/:_id" component={PlantDetails} />
-        </Switch>
+        <React.Suspense fallback={<div>{"Loading..."}</div>}>
+          <Switch>
+            <Route exact path="/" component={Plants} />
+            <Route path="/addplant" component={AddPlant} />
+            <Route path="/:_id" component={PlantDetails} />
+          </Switch>
+        </React.Suspense>
       </main>
     </BrowserRouter>
   );
