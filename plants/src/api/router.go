77665e2a6179fa350d9a08f57083ea3d2168b9a0
Lewis Llobera <billobera@gmail.com>
2020-12-21 12:17:00 +0100

    Set up for Google Cloud

--- plants/src/api/router.go
+++ plants/src/api/router.go
@@ -35,10 +35,38 @@ func Start() error {
 	fmt.Println("Plants API ready ✅")
 
 	port := os.Getenv("PLANTS_PORT")
-	err := http.ListenAndServe(":"+port, router)
+	err := http.ListenAndServe(":"+port, corsMiddleware(router))
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func corsMiddleware(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var origin string
+		if isDevelopment {
+			origin = "*"
+		} else {
+			origin = os.Getenv("WEB_PRODUCTION_URL")
+		}
+
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Origin")
+		w.Header().Add("Access-Control-Allow-Methods", "POST")
+		w.Header().Add("Access-Control-Allow-Origin", origin)
+		w.Header().Add("Access-Control-Max-Age", "86400")
+		w.Header().Add(
+			"Strict-Transport-Security",
+			"max-age=63072000; includeSubDomains; preload",
+		)
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	}
 }
--- users/src/api/router.go
+++ users/src/api/router.go
@@ -18,6 +18,7 @@ var addService = add.NewService(&storage.Inmem{})
 var listService = list.NewService(&storage.Inmem{})
 var editService = edit.NewService(&storage.Inmem{})
 var deleteService = delete.NewService(&storage.Inmem{})
+var isDevelopment = os.Getenv("MODE") == "development"
 
 // Start initializes the REST API
 func Start() error {
@@ -31,10 +32,40 @@ func Start() error {
 
 	fmt.Println("Users API ready ✅")
 
-	err := http.ListenAndServe(":"+port, router)
+	err := http.ListenAndServe(":"+port, corsMiddleware(router))
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func corsMiddleware(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var origin string
+		if isDevelopment {
+			origin = "*"
+		} else {
+			origin = os.Getenv("WEB_PRODUCTION_URL")
+		}
+
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Origin")
+		w.Header().Add(
+			"Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE",
+		)
+		w.Header().Add("Access-Control-Allow-Origin", origin)
+		w.Header().Add("Access-Control-Max-Age", "86400")
+		w.Header().Add(
+			"Strict-Transport-Security",
+			"max-age=63072000; includeSubDomains; preload",
+		)
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	}
 }
