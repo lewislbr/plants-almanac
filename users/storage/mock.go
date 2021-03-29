946914dbb73a6ae10045c03e73885fcdbec1067d
Lewis Llobera <billobera@gmail.com>
2021-03-29 20:24:22 +0200

    Add mock interfaces

--- users/storage/mock.go
+++ users/storage/mock.go
@@ -1,25 +1,23 @@
 package storage
 
-import (
-	u "users/user"
-)
+import "users/user"
 
 type MockRepo struct {
-	Users []u.User
+	Users []user.User
 }
 
-func (m *MockRepo) InsertOne(new u.User) (interface{}, error) {
+func (m *MockRepo) InsertOne(new user.User) (interface{}, error) {
 	m.Users = append(m.Users, new)
 
 	return new, nil
 }
 
-func (m *MockRepo) FindOne(email string) (u.User, error) {
+func (m *MockRepo) FindOne(email string) (user.User, error) {
 	for _, u := range m.Users {
 		if email == u.Email {
 			return u, nil
 		}
 	}
 
-	return u.User{}, u.ErrNotFound
+	return user.User{}, user.ErrNotFound
 }
